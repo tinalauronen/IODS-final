d2590b45ddcafe2ee509778930cc69670af987e4
tinalauronen <tlaurone@Tina-MacBook-Pro.local>
2017-03-06 16:02:45 +0200

    Creating the data

--- Data/creating_cudige.R
+++ Data/creating_cudige.R
@@ -1,6 +1,7 @@
 install.packages("gdata")
 library(gdata)
 library(dplyr)
+library(stringr)
 
 
 # Reading the data from excel, checking the dimensions and variable names
@@ -25,8 +26,27 @@ cudige_ <- filter(cudige9, complete.cases(cudige9))
 
 str(cudige_)
 
+# Labeling the values of the variables
+
+cudige_$Newspaper <- factor(cudige_$Newspaper, levels = c(1:6), labels = c("ABC/EP", "DN", "GU", "HS", "LM", "MIL"))
+
+cudige_$Art_form <- factor(cudige_$Art_form, levels = c(1:8), labels = c("Literature", "Popular music", "Classical music", "Film", "TV", "Theatre", "Fine arts", "Other"))
+
+cudige_$Artist_origin <- factor(cudige_$Artist_origin, levels = c(1:4), labels = c("Domestic", "Other Europe", "USA", "Other"))
+
+cudige_$Artist_time <- factor(cudige_$Artist_time, levels = c(1:4), labels = c("Present", "Post WW2", "Pre WW2", "Historical"))
+
+cudige_$Format <- factor(cudige_$Format, levels = c(1, 2), labels = c("Live", "Recording"))
+
+cudige_$Aesthetic <- factor(cudige_$Aesthetic, levels = c(0, 1), labels = c("No", "Yes"))
+
+cudige_$Commercial <- factor(cudige_$Commercial, levels = c(0, 1), labels = c("No", "Yes"))
+
+cudige_$Political <- factor(cudige_$Political, levels = c(0, 1), labels = c("No", "Yes"))
+
+
 # Saving the dataset
 
-write.csv(cudige_, "/Users/tlaurone/GitHub/tinalauronen/IODS-final/Data/cudige.csv")
+write.csv(cudige_, "/Users/tlaurone/GitHub/tinalauronen/IODS-final/Data/cudige.csv", row.names = FALSE)
 
 
